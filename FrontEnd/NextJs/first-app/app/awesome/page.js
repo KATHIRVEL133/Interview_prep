c2d6e6a35d06f62a7a0be8ffb0794d67e31f065c
KATHIRVEL133 <velkathir259@gmail.com>
2025-10-08 09:51:57 +0530

    Successfully setup the nextjs project

---
+++ FrontEnd/NextJs/first-app/app/awesome/page.js
@@ -0,0 +1,7 @@
+export default function Awesome() {
+    return <>
+    <div>
+      <h1>This is awesome page</h1>
+    </div>
+    </>
+}
---
+++ FrontEnd/NextJs/first-app/app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+       <h1>Welcome to next js project </h1>
+    </div>
+  );
+}
